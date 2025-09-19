807726973d93be7a434cc010a9780c4f01ea1ff5
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-20 00:45:09 +0530

    updating the file

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=Bharath
-person2=Rajesh
+person1=modi
+person2=putin
 echo "$person1:Hello $person2 how are you"
 echo "$person2:Hello $person1 I am fine"
 echo "$person1:From where you are coming from"

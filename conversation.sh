bd8e424dbe20976be1c1a26c19d72ecf432aa487
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-20 00:37:01 +0530

    pushing coversation file

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=Bharath
+person2=Rajesh
+echo "$person1:Hello $person2 how are you"
+echo "$person2:Hello $person1 I am fine"
+echo "$person1:From where you are coming from"
+echo "$person2: From the market"

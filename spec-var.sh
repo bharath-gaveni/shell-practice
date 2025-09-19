12bece8d9629a536beccf9db16f5b98c327fa2bf
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-20 02:00:43 +0530

    pushing file sepc-var

---
+++ spec-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "All values passed to the script:$@"
+echo "present working directory of the script:$PWD"
+echo "Home directory of the user:$HOME"
+echo "script name:$0"
+echo "who is the user:$USER"
+echo "PID of the script:$$"
+sleep 20 &
+echo "PID of last command in the background:$!"

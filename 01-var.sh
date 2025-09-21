8ffd7fe950c05b0f0d18c148544c114d58170f3b
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-21 23:08:58 +0530

    two

--- 01-var.sh
+++ 01-var.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-person1=suribabu
-person2=ramana
+person1=bujji
+person2=sravanigaru
 
-echo "$person1: Hello mr $person1 how are you"
+echo "$person1: Hello mr $person2 how are you"
 echo "$person2: I am fine $person1"
 echo "$person1: I just saw your father at market"
 echo "$person2: He just went to buy some fruits"

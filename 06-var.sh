012d67cfdd0198b0058d0ea754943ce7f55448a1
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-21 23:44:31 +0530

    six

--- 06-var.sh
+++ 06-var.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "user home directory: $HOME"
+echo "Script name: $0"
+echo "user who is running the scrip: $USER"
+echo "PID of the scrip: $$"
+echo "current working directory: $PWD"
+sleep 50 &
+echo "PID of the last executed command in the background: $!"

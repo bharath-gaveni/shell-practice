6a6039975c6a2c67369ebc3daf62fb3a40a2e0a7
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-21 22:56:06 +0530

    pusing 01-var file

---
+++ 01-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1=suribabu
+person2=ramana
+
+echo "$person1: Hello mr $person1 how are you"
+echo "$person2: I am fine $person1"
+echo "$person1: I just saw your father at market"
+echo "$person2: He just went to buy some fruits"
+

a5778a719fa4cb913d037912602906817e003b38
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-21 23:33:04 +0530

    five

---
+++ 05-var.sh
@@ -0,0 +1,11 @@
+#!/bash/bin
+
+#VAR_NAME=$(command)
+
+START_TIME=$(date +%s)
+sleep 10
+END_TIME=$(date +%s)
+TOTAL_TIME=(($END_TIME-$END_TIME))
+echo "Time taken for the script to execute $TOTAL_TIME seconds"
+
+

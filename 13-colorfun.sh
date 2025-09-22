7b74daa2cfba368875609cf7c1f2d4314b58186d
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-23 00:35:35 +0530

    13th file

---
+++ 13-colorfun.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+id=$(id -u)
+
+if [ $id -ne 0 ]; then
+    echo "ERROR: you need root access privilage to execute the script"
+    exit 1
+fi   
+
+
+VALIDATE () {
+if [ $1 -ne 0 ]; then
+    echo "ERROR: installing $2 is FAILED"
+    exit 1
+else
+    echo "installing $2 is SUCCESS"
+fi    
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    echo "installing the mysql"    
+dnf install mysql -y 
+VALIDATE $? "mysql"
+else
+    echo "skipping the mysql installation"
+fi    
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"        
+

ece63448229a73d8a90949fa7e98b072fe64ca93
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-22 23:01:34 +0530

    8th file

---
+++ 08-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+number=$1
+
+if [ $number -lt 12 ]; then
+    echo "Given number is less than 12"
+elif [ $number -eq 12 ]; then
+    echo "Given number is equal to 12"    
+else 
+    echo "Given number is greater than 12"

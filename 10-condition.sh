b056073e496ff8676081c992664c04290047827b
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-22 23:48:25 +0530

    10th file

--- 10-condition.sh
+++ 10-condition.sh
@@ -6,7 +6,7 @@ if [ $id -ne 0 ]; then
     echo "ERROR: you need root access privilage to execute the script"
     exit 1
 fi    
-dnf install mysqlserver -y 
+dnf install mysql -y 
 if [ $? -ne 0 ]; then
     echo "ERROR: installing mysql is FAILED"
 else 

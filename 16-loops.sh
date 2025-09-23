7dc9fe2eeeb1b50a19d27809fde54a794f1f8ccf
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-24 00:13:40 +0530

    loops with functions

--- 16-loops.sh
+++ 16-loops.sh
@@ -5,29 +5,38 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+Log_Folder="/var/log/shell-script"
+script_name=$( echo $0 | cut -d "." -f1 )
+Log_File="$Log_Folder/$script_name.log"
+mkdir -p $Log_Folder
+echo "Script execution start time: $(date)" | tee -a $Log_File
+
+
+
 id=$(id -u)
 
 if [ $id -ne 0 ]; then
-    echo "ERROR: you need root access privilage to execute the script"
+    echo "ERROR: you need root access privilage to execute the script" | tee -a $Log_File
     exit 1
 fi   
 
 
 VALIDATE () {
 if [ $1 -ne 0 ]; then
-    echo -e "ERROR: installing $2 is $R FAILED $N"
+    echo -e "ERROR: installing $2 is $R FAILED $N" | tee -a $Log_File
     exit 1
 else
-    echo -e "installing $2 is $G SUCCESS $N"
+    echo -e "installing $2 is $G SUCCESS $N" | tee -a $Log_File
 fi    
 }
 
 for package in {$@}
-dnf list installed $1
+dnf list installed $package &>>$Log_File
 if [ $? -ne 0 ]; then
-    echo "installing the $1"    
-dnf install $1 -y 
-VALIDATE $? "$1"
+    echo "installing the $package"    
+dnf install $package -y &>>Log_File
+VALIDATE $? $package
 else
-    echo -e " $Y skipping $N the $1 installation"
-fi    
+    echo -e " $Y skipping $N the $package` installation" | tee -a $Log_File
+fi          
+echo "Script execution end time: $(date)"

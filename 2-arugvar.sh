61db521230a49953252ba005a800f80159ddd256
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-26 21:54:20 +0530

    practice

---
+++ 2-arugvar.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi $Person2 how are you"
+echo "$Person2: Hello $Person1 I am fine"
+echo "$Person1: can we plan for a movie in X roads"
+echo "$Person2: Yeah sure $Person1 we can"

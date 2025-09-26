ac8977f8fc460803343d802ecd6f8666bd4609d2
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-26 23:45:21 +0530

    practice

--- 7-condition-numberlessthan.sh
+++ 7-condition-numberlessthan.sh
@@ -3,6 +3,8 @@ number=$1
 
 if [ $number -lt 10 ]; then
     echo "Given number is less than 10"
+elif [$number eq 10]; then
+    echo "Given number is equal to 10" 
 else
     echo "Given number is greater than 10"  
 fi      

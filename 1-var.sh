f4af1c1a41a72f4793b7b93fd258c94177f73734
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-26 21:36:48 +0530

    practice var

---
+++ 1-var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=Rajesh
+person2=Bharath
+
+Person1: Hi Person2 how are you
+Person2: Hello Person1 I am fine
+Person1: can we plan for a movie in X roads
+Person2: Yeah sure Person1 we can

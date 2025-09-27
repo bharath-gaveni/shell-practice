84dfd8c1cc00943c04d3bf724be2057583e8b96a
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-27 14:47:33 +0530

    practice

--- logs.sh
+++ logs.sh
@@ -6,35 +6,52 @@ Y="\e[0;33m"
 
 id=$(id -u)
 
+logs_folder=/var/log/shell-script
+script_name=$($0 | cut -d "." -f1)
+log_file=$logs_folder/$script_name.log
+
+mkdir -p $logs_folder
+
+echo "script execution start time: $date"
+
 if [ $id -ne 0 ]; then
-    echo "Please run this script as root user"
+    echo -e "$R Please run this script as root user $N" | tee -a $log_file
     exit 1
 fi
 
 Validate() {
     if [ $1 -ne 0 ]; then
-        echo "installing $2 is failed"
+        echo -e "installing $2 is $R failed $N" | tee -a $log_file
         exit 1
     else
-        echo "installing $2 is success"
+        echo -e "installing $2 is $G success $N" | tee -a $log_file
     fi       
 }
 
-dnf list installed nginx
+dnf list installed nginx &>>$log_file
 if [ $? -ne 0 ]; then
-dnf install nginx -y
+dnf install nginx -y &>>$log_file
 Validate $? "nginx"
 else
-    echo "nginx is already installed so skipping"
+    echo -e "nginx is already installed so $Y skipping $N" | tee -a $log_file
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>>$log_file
 if [ $? -ne 0 ]; then
-dnf install mysql -y
+dnf install mysql -y &>>$log_file
 Validate $? "mysql"
 else
-    echo "mysql is already installed so skipping"
-fi    
+    echo -e "mysql is already installed so $Y skipping $N" | tee -a $log_file
+fi
+
+dnf list installed maven &>>$log_file
+if [ $? -ne 0 ]; then
+dnf install maven -y &>>$log_file
+Validate $? "maven"
+else
+    echo -e "maven is already installed so $Y skipping $N" | tee -a $log_file
+fi
+
 
      
 

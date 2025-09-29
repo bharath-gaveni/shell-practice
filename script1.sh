47b3ab49fbefbca40bbc8581fcb9140b28f0eac7
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-29 22:31:22 +0530

    running the script

---
+++ script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+name=india
+echo "my country is $name"
+echo "PID of the script1 is: $$"
+sh script2.sh
+
---
+++ script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "my country name is: $name"
+echo "PID of the script2 is: $$"

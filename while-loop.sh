b50abe607850f6442a0efa703933885f3ff18251
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-02 15:38:43 +0530

    looping while

--- while-loop.sh
+++ while-loop.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "count begins::::"
-count=5
-while [ $count -gt 0 ]
+# echo "count begins::::"
+# count=5
+# while [ $count -gt 0 ]
+# do
+#     echo "Time left ${count}"
+#     sleep 1
+#     count=$(($count-1))
+# done
+#     echo "Your time is Up!!!!"
+
+while IFS= read -r linebyline
 do
-    echo "Time left ${count}"
-    sleep 1
-    count=$(($count-1))
-done
-    echo "Your time is Up!!!!"
+    echo "reading the file input file line by line using while loop: ${linebyline}"
+done < script1.sh
+
+

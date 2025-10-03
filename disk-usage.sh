d21ed5dd970fe6fcf43b8476f7918a5e87b7f609
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-03 19:38:17 +0530

    commit script

---
+++ disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+disk_usage=$(df-h | grep -v Filesystem)
+disk_threshold=2
+message=""
+
+while IFS= read -r linebyline 
+do
+    usage=$(echo $linebyline | awk '{print $6}' | cut -d "%" -f1)
+    partion=$(echo $linebyline | awk '{print $7}')
+    if [ $usage -ge $disk_threshold ]; then
+        message+="High Disk Usage on $partion:$usage"
+    fi  
+
+done <<< $disk_usage
+
+echo "Message Body: $mesaage"
